9ed6b34be399fab71876a0308f476fef7a712156
Pradeep Sethi <psethi@gmail.com>
2018-03-04 13:02:15 +0530

    first commit

---
+++ bot.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python3
+"""Example bot that returns a synchronous response."""
+
+from flask import Flask, request, json
+import pprint
+
+app = Flask(__name__)
+
+@app.route('/', methods=['POST'])
+def on_event():
+  """Handles an event from Hangouts Chat."""
+  event = request.get_json()
+  # pp = pprint.PrettyPrinter(depth=6)
+  # pp.pprint(event)
+  if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
+    text = 'Thanks for adding me to "%s"!' % event['space']['displayName']
+  elif event['type'] == 'MESSAGE':
+    text = 'Hello %s, How are you. You said: `%s`' % (event['message']['sender']['displayName'].split(" ")[0],event['message']['text'])
+  else:
+    return
+  return json.jsonify({'text': text})
+
+if __name__ == '__main__':
+  app.run(port=8080, debug=True)
