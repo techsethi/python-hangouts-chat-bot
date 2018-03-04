de4040b98e2cd20102bd85b25835c0f62a0ed66d
Pradeep Sethi <psethi@gmail.com>
2018-03-04 13:50:32 +0530

    using pprint & added to requirements

--- bot.py
+++ bot.py
@@ -14,8 +14,8 @@ def hello():
 def on_event():
   """Handles an event from Hangouts Chat."""
   event = request.get_json()
-  # pp = pprint.PrettyPrinter(depth=6)
-  # pp.pprint(event)
+  pp = pprint.PrettyPrinter(depth=6)
+  pp.pprint(event)
   if event['type'] == 'ADDED_TO_SPACE' and event['space']['type'] == 'ROOM':
     text = 'Thanks for adding me to "%s"!' % event['space']['displayName']
   elif event['type'] == 'MESSAGE':
